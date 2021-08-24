b687f9e5ad3aa28a8de63c93ad2ba56a53aefda4
Vadzim Vincho <vad.vincho@mail.ru>
2021-08-24 09:58:46 +0300

    changed task7

--- src/com/vadzimvincho/task7/Task7.java
+++ src/com/vadzimvincho/task7/Task7.java
@@ -3,35 +3,33 @@ package com.vadzimvincho.task7;
 // By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 // What is the 10 001st prime number?
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Task7 {
-    public int stPrime(int value) {
+
+    //  the method finds the N-th prime number
+    public int nthPrime(int nth) {
         int primeCount = 0;
-        int numCount = 0;
+        int i = 0;
         int prime = 0;
-        List<Integer> list = new ArrayList<>();
-        while (primeCount < value) {
-            if (isPrime(numCount)) {
+        while (primeCount < nth) {
+            if (isPrime(i)) {
                 primeCount++;
-                prime = numCount;
-                list.add(prime);
+                prime = i;
             }
-            numCount++;
+            i++;
         }
         return prime;
     }
 
-    private boolean isPrime(long value) {
-        if (value == 2) {
+    //    method determines whether number is prime
+    private boolean isPrime(long number) {
+        if (number == 2) {
             return true;
         }
-        if (value % 2 == 0 || value == 1) {
+        if (number % 2 == 0 || number == 1) {
             return false;
         }
-        for (int i = 3; i * i <= value; i += 2) {
-            if (value % i == 0) {
+        for (int i = 3; i * i <= number; i += 2) {
+            if (number % i == 0) {
                 return false;
             }
         }
